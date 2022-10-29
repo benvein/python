c9985f45701fac87a8d099094eaf3bbe112538b3
benvein <kokobence2006@gmail.com>
2022-10-29 19:50:36 +0200

    for ciklus 2022.10.29/1

---
+++ 07 - For Ciklus/feladat 7/main.py
@@ -0,0 +1,16 @@
+from os import system
+
+#intervallum (kezdő és végérték), csökkenő sorrend
+
+a: int = None
+b: int = None
+interval = [a, b]
+
+print("adja meg az intervallum kezdőértékét: ",end="")
+a = int(input())
+
+print("adja meg az intervallum végértékét: ",end="")
+b = int(input())
+
+for i in range(b, a, -1):
+    print(i)
