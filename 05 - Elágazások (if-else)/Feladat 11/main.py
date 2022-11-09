f9725726173d85d67ec5c0d3b21c84be93e0bbf3
benvein <114721766+benvein@users.noreply.github.com>
2022-11-09 07:38:43 +0100

    2022.11.09

--- 05 - Elágazások (if-else)/Feladat 11/main.py
+++ 05 - Elágazások (if-else)/Feladat 11/main.py
@@ -5,3 +5,29 @@ num: int = None
 print("adjon meg egy szamot: ",end="")
 num = int(input())
 
+if (num % 2 == 0 and num % 5 == 0 and 0 < num):
+    print("a szám páros, oszható 5-tel és pozitív")
+
+elif (num % 2 == 0 and num % 5 != 0 and 0 < num):
+    print("a szám páros, nem osztható 5-tel és pozitív")
+
+elif (num % 2 == 0 and num % 5 == 0 and num < 0):
+    print("a szám páros, osztható 5-tel és negatív")
+
+elif (num % 2 == 0 and num % 5 != 0 and num < 0):
+    print("a szám páros, nem oszható 5-tel és negatív")
+
+elif (num % 2 != 0 and num % 5 == 0 and 0 < num):
+    print("a szám páratlan, oszható 5-tel és pozitív")
+
+elif (num % 2 != 0 and num % 5 != 0 and 0 < num):
+    print("a szám páratlan, nem oszható 5-tel és pozitív")
+
+elif (num % 2 != 0 and num % 5 == 0 and num < 0):
+    print("a szám páratlan, oszható 5-tel és negatív")
+
+elif (num % 2 != 0 and num % 5 != 0 and num < 0):
+    print("a szám páratlan, nem osztható 5-tel és negatív")
+    
+else:
+    print("0 a szám")
