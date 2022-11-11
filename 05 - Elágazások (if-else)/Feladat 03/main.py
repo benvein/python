7d4a3a3d67305cf2a9692821776bf51263510599
benvein <114721766+benvein@users.noreply.github.com>
2022-11-11 07:44:10 +0100

    20221111

--- 05 - Elágazások (if-else)/Feladat 03/main.py
+++ 05 - Elágazások (if-else)/Feladat 03/main.py
@@ -6,6 +6,6 @@ print("adjon meg egy számot: ",end="")
 num = int(input())
 
 if (num > -30 and num < 40):
-    print=("a szám -30 és 40 között van")
+    print("a szám -30 és 40 között van")
 else: 
     print("a szám kisebb mint -30 vagy nagyobb mint 40")
--- 05 - Elágazások (if-else)/Feladat 06/main.py
+++ 05 - Elágazások (if-else)/Feladat 06/main.py
@@ -19,9 +19,9 @@ elif (num1 > num2 and num3 > num2):
     print(f"a számok sorrendben: {num2}, {num3}, {num1}")
 elif (num2 > num1 and num1 > num3):
     print(f"a számok sorrendben: {num3}, {num1}, {num2}")
-elif (num2 > num1 and num3 > num1):
+elif (num2 > num3 and num3 > num1):
     print(f"a számok sorrendben: {num1}, {num3}, {num2}")
 elif (num3 > num1 and num1 > num2):
     print(f"a számok sorrendben: {num2}, {num1}, {num3}")
-elif (num3 > num1 and num2 > num1):
+elif (num3 > num2 and num2 > num1):
     print(f"a számok sorrendben: {num1}, {num2}, {num3}")
