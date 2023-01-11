f01413f59efe9670b9a0d2b7ed32d5f58ff67104
benvein <114721766+benvein@users.noreply.github.com>
2023-01-11 07:50:49 +0100

    2023.01.11.

--- 07 - For Ciklus/Feladat 18/main.py
+++ 07 - For Ciklus/Feladat 18/main.py
@@ -1,1 +1,17 @@
 from os import system
+
+start: int = None
+end: int = None
+osszeg = 0
+
+print("kezdőérték: ",end="")
+start = int(input())
+
+print("végérték: ",end="")
+end = int(input())
+
+for i in range(start, end+1, 2):
+    osszeg += i
+for i in range(start+1, end, 2):
+    osszeg -= i
+print(osszeg)
