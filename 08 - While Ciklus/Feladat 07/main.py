58ba48dbb76586935265dff5b1ba8cdab52a0cbe
benvein <114721766+benvein@users.noreply.github.com>
2023-01-25 09:38:18 +0100

    01.25 2

--- 08 - While Ciklus/Feladat 07/main.py
+++ 08 - While Ciklus/Feladat 07/main.py
@@ -1,4 +1,19 @@
 from os import system
 system("cls")
 
-print("")
+print("adjon meg egy szamot: ",end="")
+num=int(input())
+
+print("meg egyet: ",end="")
+numMasik=int(input())
+
+while (num == numMasik):
+    print("masik szamot adjon meg: ",end="")
+    numMasik=int(input())
+
+if (num>numMasik):
+    for i in range(num, numMasik-1, -1):
+        print(i)
+else:
+    for i in range(numMasik, num-1, -1):
+        print(i)
---
+++ 08 - While Ciklus/Feladat 08/main.py
@@ -0,0 +1,8 @@
+from os import system
+system("cls")
+
+print("Pepsi [1]\nCoca Cola [2]\nSprite [3]\n Canadian dry [4]\nÁsványvíz [5]")
+
+print("adja meg melyiket szeretné: ",end="")
+valasztas=int(input())
+
