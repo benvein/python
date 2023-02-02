c0001e6a1017e3bd1a1de6e7fef50beec21f8f9b
benvein <114721766+benvein@users.noreply.github.com>
2023-02-02 13:18:23 +0100

    0202

--- 08 - While Ciklus/Feladat 10/main.py
+++ 08 - While Ciklus/Feladat 10/main.py
@@ -12,13 +12,18 @@ darabszam: int = 0
 
 for i in range(0, num, 1):
     if i % 2 == 0:
-        print(f"páros számok: {i}")
+        print("páros számok: ")
+        print(i)
     elif i % 5 == 0:
         osszeg+=i
         print(f"5-tel osztható számok összege: {osszeg}")
     elif i % 11 == 0:
         darabszam+=1
         print(f"11-gyel oszható számok darabszáma: {darabszam}")
+
+for j in range(0, num, 1):
+    if j % 7 == 3:
+        print(j)
     
 
 
